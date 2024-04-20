66ba16830222ebf556849940676807d27a4e40f4
Duvan Camilo Amaya Lopez <duvan.amayalopez@unicolombo.edu.co>
2024-04-20 13:30:23 -0500

    Guia17

---
+++ src/proyectocalculos/ProyectoCalculos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectocalculos;
+
+import PaqueteCalculos.VentanaCalculos;
+
+/**
+ *
+ * @author HP
+ */
+public class ProyectoCalculos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaCalculos ventana = new VentanaCalculos();
+        ventana.setLocationRelativeTo(ventana);
+        ventana.setVisible(true);
+    }
+    
+}
